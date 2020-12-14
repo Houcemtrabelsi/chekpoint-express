08313f83ba31d2b1fd13749f9b2ee79674775375
Trabelsi Houcem <46447553+Houcemtrabelsi@users.noreply.github.com>
2020-12-14 10:30:10 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const validateTime = require("./midlware/timer");
+app.use(express.json());
+pp.use(validateTime);
 app.listen(port, (err) => {
   err ? console.log(err) : console.log(`server running on port ${port}`);
 });
